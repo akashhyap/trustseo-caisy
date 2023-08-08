c0bf2955e97282665ceee275497cea14c36cb196
Akashdeep <akash.kashhyap@gmail.com>
2023-08-08 13:50:12 +0530

    adding headlessui/react and @heroicons/react and updating navigation

--- src/layouts/Navigation.tsx
+++ src/layouts/Navigation.tsx
@@ -1,42 +1,111 @@
 "use client";
 import Link from "next/link";
 import React from "react";
+import { useState } from 'react'
+
+import { Dialog } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+
 import { IGenNavigation } from "../services/graphql/__generated/sdk";
 import { usePathname } from "next/navigation";
 
 export const Navigation: React.FC<IGenNavigation> = ({ entries, homePage }) => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
   const slug = usePathname()?.split("/")[1];
+
   return (
-    <header className="flex flex-wrap m-10 z-50 bg-white text-sm">
-      <nav className="mx-auto" aria-label="Global">
-        <div className="flex items-end flex-col justify-between flex-end">
-          <div className="flex gap-5 flex-row flex-wrap items-center justify-center mt-0">
-            {entries?.map((entry) => {
-              if (!entry?.title || !entry.connection?.slug) {
-                return null;
-              }
-              const active =
-                entry.connection?.slug === slug ||
-                (!slug && entry.connection?.slug === homePage?.slug);
-              return (
-                <Link
-                  key={entry.id}
-                  href={"/" + entry.connection?.slug}
-                  passHref
-                  className={`font-medium ${
-                    active
-                      ? "text-blue-500"
-                      : "text-gray-600 hover:text-gray-400"
+    <header className="bg-white border-gray-200 border-b">
+      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <div className="flex lg:flex-1">
+          <Link href="/" className="-m-1.5 p-1.5">
+            <span className="sr-only">Your Company</span>
+            <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+          </Link>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            onClick={() => setMobileMenuOpen(true)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+        <div className="hidden lg:flex lg:gap-x-12">
+          {entries?.map((entry) => {
+            if (!entry?.title || !entry.connection?.slug) {
+              return null;
+            }
+            const active =
+              entry.connection?.slug === slug ||
+              (!slug && entry.connection?.slug === homePage?.slug);
+            return (
+              <Link
+                key={entry.id}
+                href={"/" + entry.connection?.slug}
+                passHref
+                className={`font-medium ${active
+                  ? "text-blue-500"
+                  : "text-gray-600 hover:text-gray-400"
                   }`}
-                  aria-current="page"
-                >
-                  {entry?.title}
-                </Link>
-              );
-            })}
-          </div>
+                aria-current="page"
+              >
+                {entry?.title}
+              </Link>
+            );
+          })}
         </div>
       </nav>
+      <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+        <div className="fixed inset-0 z-10" />
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
+              <img
+                className="h-8 w-auto"
+                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                alt=""
+              />
+            </a>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                {entries?.map((entry) => {
+                  if (!entry?.title || !entry.connection?.slug) {
+                    return null;
+                  }
+                  const active =
+                    entry.connection?.slug === slug ||
+                    (!slug && entry.connection?.slug === homePage?.slug);
+                  return (
+                    <Link
+                      key={entry.id}
+                      href={"/" + entry.connection?.slug}
+                      passHref
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                      aria-current="page"
+                    >
+                      {entry?.title}
+                    </Link>
+                  );
+                })}
+
+              </div>
+            </div>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
     </header>
   );
 };
